f4b99daad93a9015efe16b81a4ab46ddb3dcd52b
JangHyunjeong <85441226+JangHyunjeong@users.noreply.github.com>
2023-07-17 16:06:46 +0900

    useEffect1 : 기본사용법 , 왜 쓰는지? 언제 쓰는지?

--- src/pages/detail.js
+++ src/pages/detail.js
@@ -1,41 +1,49 @@
+import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 
-// styled-componets 장점
-// 1. CSS파일 안열어도 됨
-// 2.스타일이 다른 js 파일로 오염되지 않음
-// 컴포넌트.module.css 이런식으로 작명하면 걍 css써도 오염 방지 가능
-// 3. 페이지 로딩시간 단축
-
-// 단점
-// 1. js파일 매우 복잡해짐
-// 2. 중복스타일을 컴포넌트간 import할텐데 css와 다를바 없음
-// 3. 협업시 css 담당의 숙련도 이슈
-import styled from "styled-components";
-
-// 스타일이 입혀진 컴포넌트 생성하는거임.
-// let YellowBtn = styled.button`
-//   background: yellow;
-//   color: black;
-//   padding: 10px;
-// `;
-
-let YellowBtn = styled.button`
-  background: ${(props) => props.bg};
-  color: ${(props) => (props.bg === "blue" ? "white" : "black")};
-  padding: 10px;
-`;
-// 스타일 복제
-let NewBtn = styled(YellowBtn)``;
+// 컴포넌트의 Lifecycle - 이걸 배우면 이 과정에 코드실행 가능
+// 1. 페이지에 장착 mount
+// 2. 업데이트 update
+// 3. 제거 unmount
 
 function Detail(props) {
-  let { id } = useParams();
+  // #1. useEffect 쓰는 이유
+  // useEffect 안에 있는 코드는 html 랜더링 후 동작. => 오래걸리는 작업들을 useEffect안에 넣어두면, html 랜더 이후, 동작하기에 html 랜더링 속도를 더 빠르게 할 수 있다.
+  // useEffect 안에 적는 코드
+  // 1. 어려운 연산
+  // 2. 서버에서 데이터 가져오는 작업
+  // 3. 타이머 장착하는거
 
+  // #2. 이름이 Effect인 이유
+  // side Effect 코드들을 보관하는 곳이니까
+  useEffect(() => {
+    // mount, update시 여기 코드 실행
+    // 두번 실행 싫으면, index.js <React.StrictMode>없애기
+    setTimeout(() => {
+      setAlertShow(false);
+    }, 2000);
+  });
+
+  let [count, setCount] = useState(0);
+  let [alertShow, setAlertShow] = useState(true);
+  let { id } = useParams();
   return (
     <div className="container">
+      {/* 2초후 div숨기기 */}
+      {alertShow === true ? (
+        <div className="alert alert-warning">2초 이내 구매시 할인</div>
+      ) : null}
+
+      <button
+        type="button"
+        onClick={() => {
+          setCount(count + 1);
+        }}
+      >
+        {count}
+      </button>
       <div className="row">
         <div className="col-md-6">
-          <YellowBtn bg="yellow">YellowBtn</YellowBtn>
-          <NewBtn bg="blue">NewBtn</NewBtn>
           <img
             src={`https://codingapple1.github.io/shop/shoes${
               parseFloat(id) + 1
