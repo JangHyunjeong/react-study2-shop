7dcfe651b98876568f2df9434869d0fb8ae2aa7b
JangHyunjeong <85441226+JangHyunjeong@users.noreply.github.com>
2023-07-20 18:07:49 +0900

    redux5. 중복 상품 체크 추가

--- src/store.js
+++ src/store.js
@@ -6,6 +6,10 @@ let cartData = createSlice({
   initialState: [],
   reducers: {
     plus(state, action) {
+      // 인덱스 더 쉽게 찾는법 findIndex
+      // let idx = state.findIndex((a) => {
+      //   return a.id === action.payload;
+      // });
       const target = state.find((item) => {
         return item.id === action.payload;
       });
@@ -13,6 +17,17 @@ let cartData = createSlice({
       state[targetIndx].count = state[targetIndx].count + 1;
     },
     addToCart(state, action) {
+      // 중복체크
+      const overlap = state.findIndex((item) => {
+        return item.id === action.payload.id;
+      });
+      if (state.length > 0) {
+        if (overlap !== -1) {
+          alert("이미 추가된 상품입니다");
+          return state;
+        }
+      }
+      // 중복 아닐때만 실행
       const newData = {
         id: action.payload.id,
         name: action.payload.title,
